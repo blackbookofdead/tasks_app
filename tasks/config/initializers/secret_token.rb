211345c8ad8ca18a6d05b03347e8ffca75d26a9c
Fredín Fúnez <blackbookofdead@gmail.com>
2012-05-15 11:09:58 -0600

    Mi primer commit

---
+++ tasks/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '6172ae3583e3e3de1b5db47d4b834eacf8506bd9ad46816920ba3d45b85c4ab6468025b5e5542bac004b0f6cb3b6b10e4c0a8cc3467b1d4bc2fbc4f43f62178a'
